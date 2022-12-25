e15cea8cb86a607f930455537f9ee14e2ec93c66
Jordi Riera <jordiriera81@gmail.com>
2022-12-25 23:27:28 +0100

    feature:create the color styles acceptable for the directive

--- src/app/tw-button.directive.ts
+++ src/app/tw-button.directive.ts
@@ -7,35 +7,26 @@ import {
 } from '@angular/core';
 
 const Theme = {
-  primary: ['bg-blue-500', 'hover:bg-blue-700', 'text-white', 'font-bold']
+  primary: ['bg-materialPurple-primary', 'hover:bg-materialPurple-primaryLight', 'text-materialPurple-textOnPrimary','py-2', 'px-4','m-2','rounded'],
+  secondary: ['bg-materialPurple-secondary', 'hover:bg-materialPurple-secondaryLight', 'text-materialPurple-textOnSecondary','py-2', 'px-4','m-2','rounded'],
+  dark: ['bg-materialPurple-primaryDark', 'hover:bg-materialPurple-secondaryDark', 'text-materialPurple-textOnPrimary','py-2', 'px-4','m-2','rounded'],
+  error: ['bg-materialPurple-error', 'hover:bg-materialPurple-errorLight', 'text-materialPurple-textOnPrimary','py-2', 'px-4','m-2','rounded'],
+  warning: ['bg-materialPurple-warning', 'hover:bg-materialPurple-warningLight', 'text-materialPurple-textOnPrimary','py-2', 'px-4','m-2','rounded'],
 } as const
 
 
 @Directive({
-  selector: '[appTwButton]',
+  selector: '[tw-button-flat]',
+  standalone: true,
 })
 export class TwButtonDirective implements OnInit {
-@Input() colorStyle!: keyof typeof Theme; //
+@Input() colorStyle!: keyof typeof Theme; //we create a type from Theme object and map its keys to a type
 
   constructor(private hostEl:ElementRef,private renderer:Renderer2) {
     console.log(this.hostEl);
   }
 
   ngOnInit(){
-
-    //this.renderer.addClass(this.hostEl.nativeElement,Theme[this.colorStyle])
-    console.log(Object.keys(Theme));
-    console.log(Object.values(Theme));
-    const index = Object.keys(Theme).filter((key)=>{
-      return key === this.colorStyle;
-    })[0]
-    console.log(index);
-    type ThemeType = typeof Theme;
-    type ThemeKeys = keyof ThemeType;
-    const a:ThemeType = Theme;
-    const b:ThemeKeys = this.colorStyle;
-    const value = Theme[b];
-    console.log(value);
     Theme[this.colorStyle].forEach((item)=>{
       this.renderer.addClass(this.hostEl.nativeElement,item);
     });
